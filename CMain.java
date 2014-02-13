7e283a0a1e1038a49a678d8e252f090945b26b14
Jeremiah Doody <n00810032@unf.edu>
2014-02-12 23:44:32 -0500

    I wonder if this will work... ~Jeremiah

---
+++ CMain.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package network.project1;
+
+
+
+/**
+ *
+ * @author Taylor
+ */
+public class CMain
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Controller control = new Controller();
+        control.setIcon("cat.jpg");
+        
+//        Controller control1 = new Controller();
+//        control1.setIcon("cat.jpg");
+        
+    }//end main()
+   
+    
+}//end CMain class
